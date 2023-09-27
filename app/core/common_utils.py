bddd2e0143b11844dadcfb89140635fd8baa50f1
kimSangBeom <kp41589272@gmail.com>
2023-09-27 14:07:04 +0900

    incheon dust data

--- app/core/common_utils.py
+++ app/core/common_utils.py
@@ -16,6 +16,7 @@ def serviceKeyLogUpdate(db, serviceKeyData, path):
 def get_current_time():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 async def common_parameters(
-        q: Union[str, None] = None, skip: int = 0, limit: int = 10
+        # q: Union[str, None] = None, skip: int = 0, limit: int = 10
+    skip: int = 0, limit: int = 10
 ):
-    return {"q": q, "skip": skip, "limit": limit}
+    return { "skip": skip, "limit": limit}
--- app/routers/incheonAirport.py
+++ app/routers/incheonAirport.py
@@ -9,10 +9,11 @@ from apis import incheonAirportApi, authServiceKeyApi
 from schemas import incheonAirportSchemas
 
 from core.common_utils import serviceKeyLogUpdate, get_current_time, common_parameters
+import json
 
 incheon_airport_router = APIRouter(
     prefix="/api/incheon-airport",  # url 앞에 고정적으로 붙는 경로추가
-    tags=["IncheonAirPortDust"],  # docs tag name
+    tags=["인천공항 대기질 정보"],  # docs tag name
     # include_in_schema=False
 )  # Route 분리
 
@@ -25,21 +26,53 @@ def get_api_key(api_key_header: str = Depends(APIKeyHeader(name="API-KEY")), db:
     return api_key_header
 
 
-@incheon_airport_router.get("/station")
-def read_station(serviceKeyData: str = Depends(get_api_key), commons: dict = Depends(common_parameters),db: Session = Depends(get_db)):
+@incheon_airport_router.get("/station", response_model=incheonAirportSchemas.ResponseStationModel)
+def read_station(serviceKeyData: str = Depends(get_api_key), commons: dict = Depends(common_parameters),
+                 db: Session = Depends(get_db)):
     serviceKeyLogUpdate(db, serviceKeyData, '/api/airport/station')
-    total, _list = incheonAirportApi.getDustStationAllData(db=db, page=commons['skip'],size=commons['limit'])  # apis 호출
+    total, _list = incheonAirportApi.getDustStationAllData(db=db, page=commons['skip'],
+                                                           size=commons['limit'])  # apis 호출
     return {
         "status": 'success',
+        "statusCode": 200,
         "total": total,
         "data": _list,
     }  # 결과
 
 
 # 시간대 별 6개 측정소 데이터 노출 ( 시간 값이 없다면 현재 시간과 가장 가까운 시간의 데이터)
-@incheon_airport_router.get("/air-quality")
-def read_dust_all(current_time: str = get_current_time(), serviceKeyData: str = Depends(get_api_key),commons: dict = Depends(common_parameters), db: Session = Depends(get_db)):
+@incheon_airport_router.get("/air-quality", response_model=incheonAirportSchemas.ResponseDataModel)
+def read_dust_all(current_time: str = get_current_time(), serviceKeyData: str = Depends(get_api_key),
+                  db: Session = Depends(get_db)):
     serviceKeyLogUpdate(db, serviceKeyData, '/api/airport/air-quality')
-    ksb = incheonAirportApi.getDustStationDataByTime(db,current_time)
-    print(ksb)
-    return '1'
+    results = incheonAirportApi.getDustStationDataByTime(db, current_time)
+    return {
+        "status": 'success',
+        "statusCode": 200,
+        "data": results,
+    }  # 결과
+
+
+# 해당 측정소 시간대 별 대이터 노출
+@incheon_airport_router.get("/air-quality-all-data", response_model=incheonAirportSchemas.ResponseStationDataModel)
+def read_dust_station_all(station_name: str,orderby : str = 'desc', serviceKeyData: str = Depends(get_api_key),
+                          commons: dict = Depends(common_parameters), db: Session = Depends(get_db)):
+    serviceKeyLogUpdate(db, serviceKeyData, '/api/airport/air-quality-all-data')
+    total, _list = incheonAirportApi.getDustStationDataAll(station_name,orderby,db=db, page=commons['skip'],size=commons['limit'])  # apis 호출
+    return {
+        "status": 'success',
+        "statusCode": 200,
+        "total": total,
+        "data": _list,
+    }  # 결과
+
+# 해당 측정소 가장 가까운 시간의 데이터
+@incheon_airport_router.get("/air-quality-first", response_model=incheonAirportSchemas.ResponseFirstDataModel)
+def read_dust_station_first(station_name: str, serviceKeyData: str = Depends(get_api_key), db: Session = Depends(get_db)):
+    serviceKeyLogUpdate(db, serviceKeyData, '/api/airport/air-quality-first')
+    results = incheonAirportApi.getDustStationDataFirst(db,station_name)
+    return {
+        "status": 'success',
+        "statusCode": 200,
+        "data": results,
+    }  # 결과
